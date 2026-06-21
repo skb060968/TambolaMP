e41703b56ccf95c4c7683ec78d83ccd7a3143a5d
Sunil <skb060968@gmail.com>
2026-06-21 18:33:09 +0530

    Enhance Tambola ball animation with dramatic bounce and shake effects

--- public/sw.js
+++ public/sw.js
@@ -4,7 +4,7 @@
    - Cache-first for static assets (images, sounds, icons)
    ============================== */
 
-const CACHE_NAME = "tambola-mp-v28";
+const CACHE_NAME = "tambola-mp-v29";
 
 const STATIC_ASSETS = [
   "/",
